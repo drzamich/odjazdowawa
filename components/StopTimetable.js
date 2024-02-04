6c401764e4615603b599406107dd0017dac00374
Michał Drzazga <drzamich@gmail.com>
2024-02-04 18:59:24 +0100

    Add refresh button

--- components/StopTimetable.js
+++ components/StopTimetable.js
@@ -1,27 +1,32 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, Button } from "react-native";
 
 export const StopTimetable = ({ id }) => {
   const url = `https://public-sip-api.tw.waw.pl/api/GetLatestPanelPredictions?userCode=WWW&userApiKey=3aAhqA2/*RWsmvy}P8AsxgtFZ&stopId=${id}`;
 
-  const { isPending, data } = useQuery({
+  const { isPending, data, error, refetch, isRefetching } = useQuery({
     queryKey: [`timetable-${id}`],
     queryFn: () => fetch(url).then((res) => res.json()),
   });
 
-  if (isPending) {
+  if (isPending || isRefetching) {
     return <Text>Loading...</Text>;
   }
 
+  const hasData = !(!data || data.length === 0 || error);
+
   return (
     <View>
-      <FlatList
-        data={data}
-        renderItem={({ item: { Line, Destination, Arrival } }) => (
-          <Text>{`${Line} | ${Destination} | ${Arrival}`}</Text>
-        )}
-      />
+      {hasData ? (
+        <FlatList
+          data={data}
+          renderItem={({ item: { Line, Destination, Arrival } }) => (
+            <Text>{`${Line} | ${Destination} | ${Arrival}`}</Text>
+          )}
+        />
+      ) : null}
+      <Button onPress={refetch} title="Refresh" />
     </View>
   );
 };
